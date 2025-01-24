dbd7e80c88a942a150988b61d73795b9a3882cf0
nihadaslanli <nihadsasl@code.edu.az>
2025-01-24 17:15:21 +0400

    Add project files.

---
+++ Module 2 - Homework 2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Module_2___Homework_2
+{
+    internal class Program
+    {
+      
+        #region task2
+        //static void Main(string[] args)
+        //{
+        //    string word = Console.ReadLine();
+        //    string capitalizedWord = Capitalize(word);
+        //    Console.WriteLine(capitalizedWord);
+        //}
+
+        //static string Capitalize(string word)
+        //{
+        //    if (string.IsNullOrEmpty(word))
+        //        return word;
+        //    return char.ToUpper(word[0]) + word.Substring(1);
+        //}
+        #endregion
+    }
+}
